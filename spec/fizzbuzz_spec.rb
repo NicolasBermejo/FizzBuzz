9453ba89a887943ff9921a6d0f07a85f879330c4
Nicolas Bermejo <nicbermejo@gmail.com>
2021-01-25 11:12:29 +0000

    fizzbuzz end

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,19 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "fizz" when passed multiple of 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed multiple of 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed multiple of 3 & 5' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+
+it 'returns "number" when not passed multiple of 5 or 3' do
+    expect(1.fizzbuzz).to eq 1
+  end
+
 end
